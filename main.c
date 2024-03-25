daf9dc30dfde12ffa24dc49e4a08abcf1cb40c1d
AliHRadwan <alihradwan@outlook.com>
2024-03-25 23:51:55 +0200

    change and  excute the main.c file

--- main.c
+++ main.c
@@ -5,7 +5,28 @@
 
 int main (void)
 {
-    printf("Hello World!");
+    int i, row = 10, star, space;
+    
+    printf("Hello World!\n");
+
+    for (i = 1; i <= row; i++)
+    {
+
+        printf("           \n");
+
+        for (space = row; space > i; space--)
+        {
+            printf(" ");
+        }
+
+        for (star = 1; star <= ((i*2)-1); star++)
+        {
+            printf("*");
+        }
+        
+    }
+
+    printf("\n");
 
     return 0;
 }
